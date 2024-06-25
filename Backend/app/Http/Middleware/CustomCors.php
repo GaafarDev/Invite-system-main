34af5aa164c7be7c82f42794b3c059599135a25d
GaafarDev <abdullahmohmed34@gmail.com>
2024-06-26 07:15:39 +0800

    hi

---
+++ Backend/app/Http/Middleware/CustomCors.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CustomCors
+{
+    public function handle($request, Closure $next)
+    {
+        $allowedOrigins = ['https://invite-system-backend.up.railway.app', 'https://invite-system-frontend1.up.railway.app'];
+        $origin = $request->headers->get('Origin');
+
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Authorization, Origin',
+        ];
+
+        if (in_array($origin, $allowedOrigins)) {
+            $headers['Access-Control-Allow-Origin'] = $origin;
+        }
+
+        if ($request->getMethod() === 'OPTIONS') {
+            return response()->json('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+        return $response;
+    }
+}
